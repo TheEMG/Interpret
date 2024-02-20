a5130eccab038d1627dce92ce8d28b44e994783f
Eric Gutierrez <124335726+TheEMG@users.noreply.github.com>
2024-02-20 14:59:29 -0600

    Final changes
    final changes

--- Interpret.java
+++ Interpret.java
@@ -1,15 +1,20 @@
 /* Eric Gutierrez 
  * COSC 3355.001
- * Purpose: 
- * 
+ * 2/20/2024
+ * Assignment 1 
+ * Purpose: To simulate how a computer might interpret and execute Instructions (in this case 3-digit integers),and
+ * perform operations that a computer might do such as arithmetric operations, memory management between the register (1-9).
  */
 
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * The Interpret Class simulates a computer system execution of instructions, using an input file.
+ * The final output shows the final state of the register along with the counter of the executed instructions 
+ */
 class Interpret{
     //Program counter holds address of next instr 
     static int PC = 0;
@@ -30,49 +35,41 @@ class Interpret{
        
         try {
             //Create a scanner object to read from a file 
-            Scanner input = new Scanner(new File("input1-1.txt"));
+            // Scanner input = new Scanner(new File("input1-1.txt"));
+            Scanner input = new Scanner(new File("input2-1.txt"));
             int i = 0;
             
+            //Read from file 
             while (input.hasNextLine()) {
+                //Remove leading spaces 
                 String line = input.nextLine().trim();
               
-
+                //Read in values of the input file into memory array , if 3-digits are not read in
+                //then it would "pad" 0's until it meets the condition (3-digit)
                if(!line.isEmpty()){
                 String paddedLine = String.format("%03d", Integer.parseInt(line));
                 memory[i++] = Integer.parseInt(paddedLine);
                }
-            //    After loading the instructions into memory
-            // System.out.println("Initial memory content:");
-            // for (int j = 0; j < i; j++) { // Assuming 'i' is the number of loaded instructions
-            //     System.out.println("Memory[" + j + "]: " + memory[j]);
-            // }
-            
-         
-            
-           
+        
             }
-            System.out.println("Initial PC: " + PC);
-            System.out.println("Initial runBit: " + runBit);
-            PC =0;
-            while(runBit && PC < memory.length){
-              System.out.println( "memory "+memory[PC]);
-              instruction = memory[PC];
-             // System.out.println("Current instruction:" +instruction);
-              PC = PC +1;
-              int opcode = getInstructionType(instruction);
-              int operand1 = (instruction % 100)/10;
-              int operand2 = instruction % 10;
-              System.out.println(opcode + " " + operand1 + " " + operand2);
 
-       
+            while(runBit){
+
+              instruction = memory[PC];
+              PC = PC +1;
+              //Extracts the opcode
+              int opcode = getInstructionType(instruction);
+              //Extracts the first operand 
+              int operand1 = (instruction % 100)/10;
+              //Extracts the second operand 
+              int operand2 = instruction % 10;
+
               executeInstruction(opcode, operand1, operand2);
           
-
-             
               instructionCounter++;
             }
+
             output();
-            System.out.println("Final PC: " + PC + ", runBit: " + runBit);
 
             //Close file scanner 
             input.close();
@@ -82,78 +79,68 @@ class Interpret{
         }      
       
     }
+
+    /**
+     * Determines the opcode of the given instruction. 
+     * 
+     * @param instruction The instruction , which to extract the opcode. 
+     * return The opcode of the instruction 
+     */
     private static int getInstructionType(int instruction ){
         int opcode = instruction / 100;
         return opcode;
-
-
     }
-    private static void  executeInstruction (int opcode, int operand1, int operand2){
-      
 
+    /**
+     * Executes a single instruction based on opcode and the operands 
+     * 
+     * @param opcode The operation code indicating instruction type 
+     * @param operand1 The first operand 
+     * @param operand2 The second operand 
+     */
+    private static void  executeInstruction (int opcode, int operand1, int operand2){
+    
         switch (opcode) {
             // Halt 
             case 1:
-                System.out.println("Halting execution at PC=" + (PC - 1));
-
                 runBit = false;
                 break;
             // Set register "operand1" to the value of "operand2"
             case 2: 
-                System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
-
                 registers[operand1] = operand2;
                 break;
             // Multiply register "operand1" by the value "operand2"
             case 3:
-                System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
-
                 registers[operand1] = (registers[operand1] * operand2) % 1000 ;
                 break;
             // Add the value of "operand2" to the value in register "operand1" 
             case 4:
-                System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
-
                 registers[operand1] = (registers[operand1] + operand2) % 1000 ;
                 break;
             // Set register "operand1" to the value in register "operand2"
             case 5:
-                System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
- 
                 registers[operand1] = registers[operand2];
                 break;
             // Multiply register "operand1" by the value in register "operand2"
             case 6:
-                 System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
-
                  registers[operand1] = (registers[operand1] * registers[operand2]) % 1000;
                  break;
             // Add the value in register "operand2" to register "operand1"
             case 7:
-                System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
-
                 registers[operand1] = (registers[operand1] + registers[operand2])  % 1000;
                 break;
             // Set register "operand1" to the value in RAM whose address is in register "operand2"
             case 8:
-                System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
-
                 registers[operand1] = memory[registers[operand2]];
                 break;
             // Set the value in RAM whose address is in register "operand2" to that of register "operand1"
             case 9:
-               System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
- 
                 memory[registers[operand2]] = registers[operand1];
                 break;
             // Go to the location in register "operand1" unless register "operand2" contains 0 
             case 0:
-                System.out.println("Executing opcode " + opcode + " with operands " + operand1 + ", " + operand2);
-            
                 if(registers[operand2] != 0){
                     PC = registers[operand1];
-                  
-                  
                 }
                 break;
             default:
@@ -163,6 +150,9 @@ class Interpret{
 
     }
 
+    /**
+     * Outputs the final state of the registers and the total number of instruction executed 
+     */
     public static void output(){
         for (int i = 0; i < registers.length; i++){
             System.out.println("Register "+ i + ": " + registers[i]);
